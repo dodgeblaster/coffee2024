5a57967edcfc4e22eeed6c7dd7a44ce205955ba9
Gary Jennings <garysjennings@gmail.com>
2024-05-11 23:42:46 -0600

    added api template

--- apps/menu/back/logic/functions/getMenu.mjs
+++ apps/menu/back/logic/functions/getMenu.mjs
@@ -1,4 +1,4 @@
-export const config {}
+export const config = {}
 
 export const handler = async () => {
   return {
@@ -6,5 +6,11 @@ export const handler = async () => {
     body: JSON.stringify({
       items: []
     })
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Headers': 'Content-Type,Authorization',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTION'
+    }
   }
 }
